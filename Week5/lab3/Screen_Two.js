d759870b923212818538ae685382bf5cf6520ecf
BichTuyen <73806652+vttlcbt@users.noreply.github.com>
2023-10-15 20:37:11 +0700

    update week5-lab3

--- Week5/lab3/Screen_Two.js
+++ Week5/lab3/Screen_Two.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react"
 import {
   Dimensions,
   Image,
@@ -6,35 +6,24 @@ import {
   Text,
   TouchableOpacity,
   View,
-} from "react-native";
+} from "react-native"
 
-const B = (props) => {
-  const { navigation, route } = props;
-  const { navigate, goBack } = navigation;
-  const screenWidth = Dimensions.get("window").width;
-  const screenHeight = Dimensions.get("window").height;
-  const colorRef = useRef("blue");
-  const [color, setColor] = useState("blue");
+const choiceColor = (props) => {
+  const { navigation, route } = props
+  const { navigate, goBack } = navigation
+  const screenWidth = Dimensions.get("window").width
+  const screenHeight = Dimensions.get("window").height
+  const colorRef = useRef("blue")
+  const [color, setColor] = useState("blue")
 
-  useEffect(() => {}, []);
+  useEffect(() => {}, [])
 
   const [imageSource, setImageSource] = useState(
     require("../assets/mobile4.png")
-  );
+  )
   return (
-    <View
-      style={{
-        height: screenHeight,
-        width: screenWidth,
-      }}
-    >
-      <View
-        style={{
-          height: screenHeight * 0.25,
-          width: screenWidth,
-          flexDirection: "row",
-        }}
-      >
+    <View style={styles.container}>
+      <View style={styles.header}>
         <Image
           source={
             colorRef.current === "white"
@@ -47,218 +36,192 @@ const B = (props) => {
               ? require("../assets/mobile4.png")
               : require("../assets/mobile4.png")
           }
-          style={{
-            height: screenHeight * 0.25,
-            width: screenWidth * 0.38,
-            resizeMode: "contain",
-          }}
+          style={styles.img_phone}
         />
         <View
           style={{
             marginHorizontal: 20,
             marginVertical: 15,
-            flex: 1,
           }}
         >
-          <Text
-            style={{
-              fontSize: 15,
-              fontWeight: "500",
-              color: "black",
-            }}
-          >
-            Điện Thoại Vsmart Joy 3
-          </Text>
-          <Text
-            style={{
-              fontSize: 15,
-              fontWeight: "500",
-              color: "black",
-            }}
-          >
-            Hàng chính hãng
-          </Text>
+          <Text style={styles.text1}>Điện Thoại Vsmart Joy 3</Text>
+          <Text style={styles.text1}>Hàng chính hãng</Text>
 
-          <Text
-            style={{
-              fontSize: 15,
-              fontWeight: "500",
-              color: "black",
-              marginVertical: 13,
-            }}
-          >
-            Màu:
-            <Text
-              style={{
-                fontSize: 15,
-                fontWeight: "700",
-                color: "black",
-              }}
-            >
-              {" " + color}
-            </Text>
-          </Text>
-          <Text
-            style={{
-              fontSize: 15,
-              fontWeight: "500",
-              color: "black",
-            }}
-          >
-            Cung cấp bởi{" "}
-            <Text
-              style={{
-                fontSize: 15,
-                fontWeight: "700",
-                color: "black",
-              }}
-            >
-              Tiki Tradding
-            </Text>
-          </Text>
-          <Text
-            style={{
-              marginTop: 13,
-              fontSize: 18,
-              fontWeight: "700",
-              color: "black",
-            }}
-          >
-            1.790.000 đ
-          </Text>
+          <View style={styles.info_spp}>
+            <Text style={styles.text1}>Màu:</Text>
+            <Text style={styles.text2}>{" " + color}</Text>
+          </View>
+
+          <View style={styles.info_spp}>
+            <Text style={styles.text1}>Cung cấp bởi: </Text>
+            <Text style={styles.text2}>Tiki Tradding</Text>
+          </View>
+
+          <Text style={styles.text3}>1.790.000 đ</Text>
         </View>
       </View>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "#c4c4c4",
-        }}
-      >
-        <Text
-          style={{
-            marginHorizontal: 15,
-            marginTop: 11,
-            fontSize: 15,
-            fontWeight: "400",
-          }}
-        >
-          Chọn 1 màu bên dưới
-        </Text>
+      <View style={styles.chonMau}>
+        <Text style={styles.text1}>Chọn 1 màu bên dưới</Text>
 
         <TouchableOpacity
           onPress={() => {
-            colorRef.current = "white";
-            setColor("white");
-            setImageSource(require("../assets/mobile1.png"));
+            colorRef.current = "white"
+            setColor("white")
+            setImageSource(require("../assets/mobile1.png"))
           }}
         >
-          <View
-            style={{
-              marginTop: 11,
-              height: screenHeight * 0.13,
-              width: screenHeight * 0.13,
-              backgroundColor: "#c5f1fb",
-              alignSelf: "center",
-            }}
-          ></View>
+          <View style={styles.viewWhite}></View>
         </TouchableOpacity>
 
         <TouchableOpacity
           onPress={() => {
-            colorRef.current = "red";
-            setColor("red");
-            setImageSource(require("../assets/mobile2.png"));
+            colorRef.current = "red"
+            setColor("red")
+            setImageSource(require("../assets/mobile2.png"))
           }}
         >
-          <View
-            style={{
-              marginTop: 8,
-              height: screenHeight * 0.13,
-              width: screenHeight * 0.13,
-              backgroundColor: "red",
-              alignSelf: "center",
-            }}
-          ></View>
+          <View style={styles.viewRed}></View>
         </TouchableOpacity>
 
         <TouchableOpacity
           onPress={() => {
-            colorRef.current = "black";
-            setColor("black");
-            setImageSource(require("../assets/mobile3.png"));
+            colorRef.current = "black"
+            setColor("black")
+            setImageSource(require("../assets/mobile3.png"))
           }}
         >
-          <View
-            style={{
-              marginTop: 8,
-              height: screenHeight * 0.13,
-              width: screenHeight * 0.13,
-              backgroundColor: "black",
-              alignSelf: "center",
-            }}
-          ></View>
+          <View style={styles.viewBlack}></View>
         </TouchableOpacity>
 
         <TouchableOpacity
           onPress={() => {
-            colorRef.current = "blue";
-            setColor("blue");
-            setImageSource(require("../assets/mobile4.png"));
+            colorRef.current = "blue"
+            setColor("blue")
+            setImageSource(require("../assets/mobile4.png"))
           }}
         >
-          <View
-            style={{
-              marginTop: 8,
-              height: screenHeight * 0.13,
-              width: screenHeight * 0.13,
-              backgroundColor: "#234896",
-              alignSelf: "center",
-            }}
-          ></View>
+          <View style={styles.viewBlue}></View>
         </TouchableOpacity>
 
         <TouchableOpacity
           onPress={() => {
             navigate("Screen_One", {
               color: colorRef.current,
-            });
+            })
           }}
         >
-          <View
-            style={{
-              borderRadius: 10,
-              backgroundColor: "rgba(25, 82, 226, 0.58)",
-              borderStyle: "solid",
-              borderColor: "#ca1536",
-              borderWidth: 1,
-              height: screenHeight * 0.07,
-              width: screenWidth * 0.9,
-              alignSelf: "center",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: 15,
-              shadowColor: "rgba(0, 0, 0, 0.25)",
-              shadowOffset: { width: 0, height: 4 },
-              shadowOpacity: 0.5,
-              shadowRadius: 4,
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 20,
-                color: "#f9f2f2",
-                textAlign: "left",
-              }}
-            >
-              XONG
-            </Text>
+          <View style={styles.btnXong}>
+            <Text style={styles.text4}>XONG</Text>
           </View>
         </TouchableOpacity>
       </View>
     </View>
-  );
-};
+  )
+}
 
-export default B;
+export default choiceColor
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  btnXong: {
+    borderRadius: 10,
+    backgroundColor: "rgba(25, 82, 226, 0.58)",
+    borderStyle: "solid",
+    borderColor: "#1521CA",
+    borderWidth: 1,
+    height: "50px",
+    width: "100%",
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 15,
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+  },
+  text4: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#f9f2f2",
+    textAlign: "center",
+  },
+  viewWhite: {
+    marginTop: 10,
+    // height: "screenHeight * 0.13",
+    // width: "screenHeight * 0.13",
+    backgroundColor: "#c5f1fb",
+    alignSelf: "center",
+    width: 90,
+    height: 90,
+  },
+  viewRed: {
+    marginTop: 11,
+    // height: "screenHeight * 0.13",
+    // width: "screenHeight * 0.13",
+    backgroundColor: "red",
+    alignSelf: "center",
+    width: 90,
+    height: 90,
+  },
+  viewBlack: {
+    marginTop: 11,
+    // height: "screenHeight * 0.13",
+    // width: "screenHeight * 0.13",
+    backgroundColor: "black",
+    alignSelf: "center",
+    width: 90,
+    height: 90,
+  },
+  viewBlue: {
+    marginTop: 11,
+    // height: "screenHeight * 0.13",
+    // width: "screenHeight * 0.13",
+    backgroundColor: "blue",
+    alignSelf: "center",
+    width: 90,
+    height: 90,
+  },
+  chonMau: {
+    flex: 1,
+    backgroundColor: "#c4c4c4",
+    padding: 10,
+    // alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  container: {
+    height: "screenHeight",
+    width: "screenWidth",
+  },
+  header: {
+    height: "screenHeight * 0.25",
+    width: "screenWidth",
+    flexDirection: "row",
+  },
+  img_phone: {
+    resizeMode: "contain",
+    width: "38%",
+    height: "90%",
+    alignSelf: "center",
+  },
+  text1: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "black",
+  },
+  text2: {
+    fontSize: 15,
+    fontWeight: "700",
+    color: "black",
+  },
+  text3: {
+    marginTop: 13,
+    fontSize: 18,
+    fontWeight: "700",
+    color: "black",
+  },
+  info_spp: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginTop: 5,
+  },
+})
